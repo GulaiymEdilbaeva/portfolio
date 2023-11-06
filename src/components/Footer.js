0201b0e7b3cd1e584449a73ff9dfb20fe4afc575
GULAIYM EDILBAEVA <gulaiymhope@gmail.com>
2023-11-06 18:19:38 +0600

    save

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Frontend Developer Gulaiym Edilbaeva</h3>
+          <h3>Frontend developer Gulaiym Edilbaeva</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} </h3>
